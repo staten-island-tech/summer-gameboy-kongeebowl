f4924f5179958fd8fad78b62c653fdad2ded49d5
kongeebowl <kadenpang09@gmail.com>
2026-07-20 08:07:12 -0400

    did stuff

--- sprite/test.c
+++ sprite/test.c
@@ -1,10 +1,11 @@
 #include "smirk.h"
 #include <gb/gb.h>
 
-void main() {
+int main() {
   SPRITES_8x16;
   set_sprite_data(0, 8, smirk);
   set_sprite_tile(0, 0);
   move_sprite(0, 75, 75);
   SHOW_SPRITES;
+  return 0;
 }
